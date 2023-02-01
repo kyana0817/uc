eb6e8ee42d39c05e2956c8667100e82f76ebef65
kazeyanagi <kyana0817@gmail.com>
2023-02-01 14:40:43 +0900

    :sparkles: features pipeline

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,29 @@ use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
 const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
 
 struct Argument {
-    mode: String,
     input: Option<String>,
 }
 
 fn main() {
     let args: Argument = parce_args();
-    let encoded = encode(&args.input
-        .expect("error not input")
-    );
 
-    println!("{}", args.mode);
-    println!("encode: {}", encoded);
+    match args.input {
+        Some(input) => encode(&input),
+        None => io_pipe(),
+    }
 }
 
 fn parce_args() -> Argument {
-    let args: Vec<String> = env::args().skip(1).collect();
-
-    if args.len() != 2 {
-        eprintln!("{} wrong number of arguments: expected 4, get {}", "Error:".red().bold(), args.len());
-        std::process::exit(1);
-    }
+    let args = env::args().nth(1);
 
     Argument {
-        mode: args[0].clone(),
-        input: Some(args[1].clone()),
+        input: args,
     }
 }
 
-fn encode(input: &str) -> String {
-    utf8_percent_encode(input, FRAGMENT).to_string()
+fn encode(input: &str) {
+    let encoded = utf8_percent_encode(input, FRAGMENT).to_string();
+    println!("{}", encoded);
 }
 
 fn io_pipe() {
@@ -43,12 +36,13 @@ fn io_pipe() {
         io::stdin()
             .read_line(&mut input)
             .expect("failed to read pipe");
-    
-        input = input.trim().to_string();
-    
+        
         if input == "" {
             break;
         }
-        println!("Pipe output: {}", input);
+
+        input = input.trim().to_string();
+            
+        encode(&input);
     }
 }
