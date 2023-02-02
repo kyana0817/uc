851e4aec207bbbaa6cf742abf77e3ae4b96a377f
kazeyanagi <kyana0817@gmail.com>
2023-02-02 23:16:38 +0900

    :fire: remove files

--- src/converts/decode.rs
+++
@@ -1,5 +0,0 @@
-use percent_encoding::{percent_decode_str};
-
-pub fn decode(input: &str) -> String {
-    return percent_decode_str(input).decode_utf8().expect("decode error").to_string();
-}
--- src/converts/encode.rs
+++
@@ -1,7 +0,0 @@
-use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
-
-const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
-
-pub fn encode(input: &str) -> String {
-    return utf8_percent_encode(input, FRAGMENT).to_string();
-}
